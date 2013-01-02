562afad2b7158109b3a2e786fc5548279e2f5613
Anuj More <anujmorex@gmail.com>
2013-01-02 19:27:31 +0530

    Adding styling guide for Python 3

---
+++ python3/000-styling.py
@@ -0,0 +1,10 @@
+#/usr/bin/env python
+"""
+From: <insert link here>
+<insert description here>
+
+Requrements:
+<insert commands to execute to satisfy requirements here>
+"""
+
+# <insert code here>
