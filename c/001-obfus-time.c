023449b536586a95005ad2f04a5068fa7c075f8d
Anuj More <anujmorex@gmail.com>
2013-04-19 16:01:08 +0530

    Obfuscated date-time display

---
+++ c/001-obfus-time.c
@@ -0,0 +1,9 @@
+/*
+From: http://stackoverflow.com/questions/15393441/obfuscated-c-code-contest-2006-please-explain-sykes2-c
+
+Compiling instructions:
+gcc -o time 001-obfus-time.c
+*/
+
+main(_){_^448&&main(-~_);putchar(--_%64?32|-~7[__TIME__-_/8%8][">'txiZ^(~z?"-48]>>";;;====~$::199"[_*2&8|_/64]/(_&2?1:8)%8&1:10);}
+
