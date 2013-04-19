e0a41f60bff2a83e6cca6bc0110d434dbc0856f7
Anuj More <anujmorex@gmail.com>
2013-04-19 16:00:37 +0530

    Adding a styling guide to C folder

---
+++ c/000-styling.c
@@ -0,0 +1,10 @@
+#include <...>
+/*
+From: <insert link here>
+<insert description here>
+
+Compiling instructions:
+<insert commands to execute to satisfy requirements here>
+*/
+
+/* Insert code here */
