8e4efd378c87bb95e2a0f2c1fa1d10e1ef0baf8f
Anuj More <anujmorex@gmail.com>
2013-05-29 23:06:25 +0530

    Faster (but at times inaccurate implementation) of a spellcheck

---
+++ python3/cpellsheck-twitch/cpellsheck-better.py
@@ -0,0 +1,68 @@
+# cpellsheck-lolrus.py
+# Populated with words after calling prepare_wordlist()
+words = dict()
+
+def process_word(word):
+    # Lowercase
+    word.lower()
+
+    # Remove repetitions
+    ulist = []
+    # [ulist.append(f) AND prev = f for f in word if f not prev]
+    prev = ''
+    ulist = []
+    for f in word:
+        if f is not prev:
+            ulist.append(f)
+            prev = f
+
+    print(ulist)
+
+    # Remove vowels
+    prev = ''
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    vlist = []
+    for f in ulist:
+        if f not in vowels:
+            vlist.append(f)
+            prev = None
+        elif prev is not '*':
+            vlist.append('*')
+            prev = '*'
+
+    return ''.join(vlist)
+
+def prepare_wordmap():
+    # Use the /usr/share/dict/words (~120000 words) OR
+    # The file containing top 5k words in English from http://www.englishclub.com/vocabulary/common-words-5000.htm OR
+    # The file containing top 10k words in English from https://github.com/first20hours/google-10000-english
+    wordmap = {}
+    f1 = open('words-5k')
+
+    alist = f1.readlines()
+    blist = [f.replace('\n', '') for f in alist]
+
+    for f in blist:
+        wordmap[process_word(f)] = f
+    return wordmap
+
+def correct(word):
+    print("Word1: %s" % word)
+    if word in words.values():
+        return word
+    if word not in words:
+        word = process_word(word)
+        print("Word2: %s" % word)
+        if word in words.keys():
+            return words[word]
+        else:
+            return 'NO SUGGESTION'
+
+if __name__ == "__main__":
+    words = prepare_wordmap()
+
+    while True:
+        word = input("Enter a word ('quit' to quit): ")
+        if word == "quit":
+            break
+        print(correct(word.lower()))
