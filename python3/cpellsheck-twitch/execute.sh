dd0ebaea4ea464d69426640c3b0ee181896df656
Anuj More <anujmorex@gmail.com>
2013-05-30 00:46:12 +0530

    Moving unnecessary files to dump, and renaming execute to execute.sh

---
+++ python3/cpellsheck-twitch/execute.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 1. Run cpellerror and store the original words (for later comparison) in
+# original file. Pass the results (error words) to cpellsheck and redirect the
+# results to ans file
+python cpellerror-better.py 2> original | python cpellsheck-better.py > ans
+
+# 2. Filter answers from "> " and store that to answer file. Remove ans file.
+cat ans | cut -d" " -f2 > answer
+rm ans
+
+# Find out the difference between original (created in #1) and answer (created
+# in #2)
+diff original answer
+
+# Because the process involves taking inverse function of a function
+# (correcting an induced spelling error), the answer should have no incorrect
+# corrections. But the map being a one to many relation, calculating inverse
+# correctly each time is not possible.
+echo "False corrections: " `diff original answer | grep '>' | wc -l`
+echo "No suggestions: " `diff original answer | grep 'NO' | wc -l`
